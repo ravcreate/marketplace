cea24106f722f819e40328ed6a712ecd7ff597d1
ravcreate <ravuthvann@outlook.com>
2024-06-10 13:42:37 -0700

    Added Kinde Authentication and User Profile Menu

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,10 +1,29 @@
+"use client";
+
+import { useEffect, useState } from "react";
 import Link from "next/link";
 
 import { NavLinks } from "./navbar-links";
 import { Button } from "@/components/ui/button";
 import { MobileMenu } from "./mobile-menu";
+import {
+    LoginLink,
+    LogoutLink,
+    RegisterLink,
+} from "@kinde-oss/kinde-auth-nextjs";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
+import { UserNav } from "./user-nav";
 
+/**
+ *	Component Starts Here
+ */
 export const Navbar = () => {
+    const { user, isLoading } = useKindeBrowserClient();
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <nav className="relative max-w-7xl w-full flex  items-center px-4 md:grid md:grid-cols-12 md:px-8 mx-auto py-7">
             <div className="md:col-span-3">
@@ -14,8 +33,25 @@ export const Navbar = () => {
             </div>
             <NavLinks />
             <div className="flex items-center gap-x-2 ms-auto md:col-span-3">
-                <Button variant="secondary">Sign In</Button>
-                <Button>Sign Up</Button>
+                {user ? (
+                    <UserNav
+                        email={user.email as string}
+                        name={user.given_name as string}
+                        userImage={
+                            user.picture ??
+                            `https://avatar.vercel.sh/${user.given_name}`
+                        }
+                    />
+                ) : (
+                    <div className="flex items-center gap-x-2">
+                        <Button variant="secondary" asChild>
+                            <LoginLink>Sign In</LoginLink>
+                        </Button>
+                        <Button asChild>
+                            <RegisterLink>Sign Up</RegisterLink>
+                        </Button>
+                    </div>
+                )}
 
                 <div className="md:hidden">
                     <MobileMenu />
---
+++ app/components/user-nav.tsx
@@ -0,0 +1,58 @@
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { Button } from "@/components/ui/button";
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs";
+import { AvatarImage } from "@radix-ui/react-avatar";
+import { DropdownMenuItem } from "@radix-ui/react-dropdown-menu";
+
+interface UserNavProps {
+    email: string;
+    name: string | undefined;
+    userImage: string | undefined;
+}
+
+export const UserNav = ({ email, name, userImage }: UserNavProps) => {
+    return (
+        <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+                <Button
+                    variant="ghost"
+                    className="relative h-10 w-10 rounded-full"
+                >
+                    <Avatar>
+                        <AvatarImage src={userImage} alt="User Image" />
+                        <AvatarFallback>{name}</AvatarFallback>
+                    </Avatar>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56" align="end" forceMount>
+                <DropdownMenuLabel className="font-normal">
+                    <div className="flex flex-col space-y-1">
+                        <p className="text-sm font-medium">{name}</p>
+                        <p className="text-xs leading-none text-muted-foreground">
+                            {email}
+                        </p>
+                    </div>
+                </DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuGroup>
+                    <DropdownMenuItem>Test Item</DropdownMenuItem>
+                    <DropdownMenuItem>Test Item</DropdownMenuItem>
+                    <DropdownMenuItem>Test Item</DropdownMenuItem>
+                    <DropdownMenuItem>Test Item</DropdownMenuItem>
+                </DropdownMenuGroup>
+                <DropdownMenuSeparator />
+                <Button variant="secondary" asChild>
+                    <LogoutLink>Sign Out</LogoutLink>
+                </Button>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    );
+};
